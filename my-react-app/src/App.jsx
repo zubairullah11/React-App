c95eae3421fad3fc384e529e5b0126f7f12e5c96
Zubairullah11 <zubairullah561@gmail.com>
2025-08-08 21:28:58 +0500

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="my-heading">Welcome to my React app!</h1>
+    </div>
+  );
+}
+
+export default App;
